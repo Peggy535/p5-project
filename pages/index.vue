034ec7e73067a4b47f868a5b050354ed70bed016
Wayne Pegg <peggy535@icloud.com>
2024-11-07 11:53:00 +0000

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref, onMounted } from "vue";
+
+const p5Container = ref(null);
+const { $p5 } = useNuxtApp();
+onMounted(() => {
+  new $p5((p) => {
+    p.setup = () => {
+      p.createCanvas(p.windowWidth, p.windowHeight);
+    };
+    p.draw = () => {
+      p.background("navy"); //navy background
+    };
+  }, p5Container.value);
+});
+</script>
+<template>
+  <div>
+    <main class="w-screen min-h-dvh overflow-x-hidden relative" role="main">
+      <div
+        ref="p5Container"
+        class="absolute top-0 left-0 w-full h-full z-10"
+      ></div>
+    </main>
+  </div>
+</template>
